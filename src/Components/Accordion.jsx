6e752567f6205d92b3b48612b3700ae563b31195
notscorpio91 <notscorpio91@gmail.com>
2023-10-31 19:32:45 +0500

    Accordion added

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import plus from '../assets/svg/plus.svg'
+import React, {useState} from 'react'
+import  plus  from '../assets/svg/plus.svg'
+import minus from '../assets/svg/minus.svg'
+
 
 const Accordion = () => {
+
+const [show,setShow]=useState(true)
+const toggleFunction = ()=>{
+  setShow(!show)
+}
+
   return (
-    <div className='accordion'>
-      <div className='iten'>
-        <p className="number">number</p>
-        <p className="text">question</p>
-        <img src={plus} alt="plus" />
-        <div className="hidden-box">
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, id quo maxime dolore provident in culpa eaque magnam corrupti quibusdam! Officia ut dicta maxime veniam ipsa doloribus assumenda fugit molestiae.</p>
+    <div className='accordion  flex flex-col justify-center items-center pt-4 h-[40vh]'>
+      <div className=" w-[540px] h-[40px] pt-[12px]  rounded-[5px] bg-[#18181C] ">
+      <div className=' flex justify-between items-left mx-[28px] '>
+        <h1 className='text-[#FFF] font-poppins font-normal leading-5 text-sm'>How promote the product?</h1>
+        <button onClick={toggleFunction}><img src={show ? minus : plus} alt="plus" /></button>
         </div>
+
+        { show? <div className='h-[130px] bg-[#18181c]  rounded-[5px] flex  items-center '>
+         <p className='text-sm leading-[28px] font-poppins font-normal text-[#9E9E9E] mx-[28px] border-t border-[#404047] '>A good design is not only aesthetically pleasing, but also functional. It should be able to solve the problem  good design is not only aesthetically pleasing, but also functional. </p> 
+      </div>:null}
+
+
       </div>
     </div>
   )
