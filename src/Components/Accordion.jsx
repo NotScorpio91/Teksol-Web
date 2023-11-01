f21bb5369a073b915c551ea497afa3bcc71eb55b
NotScorpio91 <[notscorpio91@gmail.com]>
2023-11-01 13:05:31 +0500

    All components done

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -11,16 +11,16 @@ const toggleFunction = ()=>{
 }
 
   return (
-    <div className='accordion  flex flex-col justify-center items-center pt-4 h-[40vh]'>
-      <div className=" w-[540px] h-[40px] pt-[12px]  rounded-[5px] bg-[#18181C] ">
-      <div className=' flex justify-between items-left mx-[28px] '>
+    <div className='accordion  flex flex-col justify-center items-center pt-4 h-[40vh] '>
+      <div className=" w-[540px] h-[40px] pt-[12px]  rounded-[5px] bg-[#18181C]  ">
+      <div className=' flex justify-between items-left mx-[28px]  '>
         <h1 className='text-[#FFF] font-poppins font-normal leading-5 text-sm'>How promote the product?</h1>
-        <button onClick={toggleFunction}><img src={show ? minus : plus} alt="plus" /></button>
+        <button onClick={toggleFunction}><img src={show ? plus : minus} alt="plus" /></button>
         </div>
 
-        { show? <div className='h-[130px] bg-[#18181c]  rounded-[5px] flex  items-center '>
-         <p className='text-sm leading-[28px] font-poppins font-normal text-[#9E9E9E] mx-[28px] border-t border-[#404047] '>A good design is not only aesthetically pleasing, but also functional. It should be able to solve the problem  good design is not only aesthetically pleasing, but also functional. </p> 
-      </div>:null}
+        <div className={` accor  h-[130px] bg-[#18181c]  rounded-[5px] flex  items-center  ${show ? 'hidden': 'block'}`}>
+         <p className='text-sm leading-[28px] font-poppins font-normal text-[#9E9E9E] mx-[28px] border-t border-[#404047] pt-3 '>A good design is not only aesthetically pleasing, but also functional. It should be able to solve the problem  good design is not only aesthetically pleasing, but also functional. </p> 
+      </div>
 
 
       </div>
