759bb66ea7c80d401646ca76d1f9105834b2501f
notscorpio91 <notscorpio91@gmail.com>
2023-11-04 18:53:17 +0500

    Animation added in Cards

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,7 @@ import  svg  from '../assets/svg/svg.svg'
  
 function Card() {
   return (
-    <div className='text-white my-5' >
+    <div className='text-white my-5 transition-all active:scale-95' >
     <div className=' bg-[#18181C] w-[350px] h-[371px] rounded-2xl flex justify-center flex-col items-center space-y-8'>
       <img className='w-[104px] h-[104px] pb-7' src={svg} alt="" />
              <h1 className='text-white font-semibold font-poppins antialiased '>Fully Customizable</h1>
