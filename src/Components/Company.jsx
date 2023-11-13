0035c310e877be2d12c3bfac95e97d92ac3e66b8
NotScorpio91 <notscorpio91@gmail.com>
2023-11-13 16:14:26 +0500

    Dropdown some issue fixed

--- src/Components/Company.jsx
+++ src/Components/Company.jsx
@@ -17,10 +17,10 @@ const solutions = [
 
 export default function Company() {
   return (
-    <Popover  >
-      <Popover.Button className="inline-flex items-center  gap-x-1 text-sm font-semibold leading-6 text-[#9E9E9E] hover:text-white outline-none ">
-        <span>Company</span>
-        <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />
+    <Popover >
+      <Popover.Button className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-[#9E9E9E] hover:text-white outline-none  group  ">
+        <span>Services</span>
+        <ChevronDownIcon className="h-5 w-5  " aria-hidden="true" />
       </Popover.Button>
 
       <Transition
@@ -32,13 +32,13 @@ export default function Company() {
         leaveFrom="opacity-100 translate-y-0"
         leaveTo="opacity-0 translate-y-1"
       >
-        <Popover.Panel className="absolute  top-[69px]  z-10  flex -ml-[130px]  px-4">
-          <div className="w-fit flex-auto  overflow-hidden rounded-[5px] bg-black bg-opacity-90  backdrop-blur-md text-base leading-6 shadow-lg ring-1 ring-gray-800">
-            <div className="p-4 grid grid-cols-2  ">
-              {solutions.map((item) => (           
-                <div key={item.name} className="group relative flex   items-center gap-x-6 rounded-[5px] px-4 py-2 mb-1 hover:text-white  ">
-                  <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg  group-hover:bg-gradient-to-r from-[#00c6ff] to-[#92EFFD] hover:transition-all ">
-                    <item.icon className="h-6 w-6 text-[#9E9E9E] group-hover:text-black" aria-hidden="true" />
+        <Popover.Panel className="absolute top-[69px]  z-10  flex -ml-[22px]  px-4 backdrop:blur-md ">
+          <div className="w-fit flex-auto  overflow-hidden rounded-[5px] bg-black bg-opacity-95 backdrop:blur-md text-base leading-6 shadow-lg ring-1 ring-[#0A0A0A] ">
+            <div className="p-1 grid grid-cols-2 ">
+              {solutions.map((item) => (
+                <div key={item.name} className="group relative flex items-center   p-1 hover:text-white hover:bg-[#0A0A0A] rounded-md  ">
+                  <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg   hover:transition-all">
+                    <item.icon className="h-6 w-6 text-[#9E9E9E] group-hover:text-white" aria-hidden="true" />
                   </div>
                   <div>
                     <a href={item.href} className="font-semibold text-[#9E9E9E] hover:text-white">
