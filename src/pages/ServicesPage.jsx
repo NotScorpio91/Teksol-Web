15919adbae94ef39f71b7ede833d7382866c85dc
DarkcoderSe <kashifs294@gmail.com>
2023-11-26 18:24:34 +0500

    service-page completed

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -20,7 +20,9 @@ const services = [
         tagline: 'We are Web Development Experts.',
         projects: [
             {
+				textColor: 'text-white',
                 class: 'col-span-2 !bg-[#242532]',
+				imageClass: 'absolute w-[400px] right-6 -bottom-12',
                 title: 'Social Currant',
                 slug: 'social-currant',
                 image: pI_sc_image,
@@ -48,13 +50,15 @@ const services = [
                 ],
             },
             {
-                class: '',
+				textColor: '!text-black',
+                class: '!bg-blue-100',
+				imageClass: 'absolute w-[400px] right-6 -bottom-12',
                 title: 'Kikboss',
                 slug: 'kikboss',
                 image: '',
                 logo: pI_kb_logo,
                 description:
-                    'KiKBoss is a platform that is bridging the gaps, connecting sellers to potential buyers, and making it easy to buy and sell from anywhere, anytime online.',
+                    'KiKBoss is a platform, connecting sellers to potential buyers, and making it easy to buy and sell from anywhere, anytime online.',
                 tech: [
                     {
                         name: 'Laravel',
@@ -67,13 +71,15 @@ const services = [
                 ],
                 links: [
                     {
-                        name: 'Demo',
+                        name: 'Visit the website',
                         href: 'https://kikboss.com/',
                     },
                 ],
             },
             {
+				textColor: 'text-white',
                 class: '',
+				imageClass: 'absolute w-[230px] right-10 -bottom-20',
                 title: 'DentiNect',
                 slug: 'dentinect',
                 image: '',
@@ -92,19 +98,21 @@ const services = [
                 ],
                 links: [
                     {
-                        name: 'Demo',
+                        name: 'Visit the website',
                         href: 'https://dentinect.co/',
                     },
                 ],
             },
             {
-                class: 'col-span-2',
+				textColor: 'text-white',
+                class: 'col-span-2 !bg-gray-800',
+				imageClass: 'absolute w-[400px] right-16 -bottom-36',
                 title: 'Earnfluencers',
                 slug: 'earnfluencers',
                 image: pI_ef_image,
                 logo: '',
                 description:
-                    'Connect with brands, showcase your talent, and monetize your content. Join our community of successful TikTokers and turn your passion into profit. Sign up today and unlock your earning potential!',
+                    'Connect with brands, showcase your talent, and monetize your content. Join our community of successful TikTokers and turn your passion into profit.',
                 tech: [
                     {
                         name: 'React',
@@ -117,13 +125,15 @@ const services = [
                 ],
                 links: [
                     {
-                        name: 'Demo',
+                        name: 'Visit the website',
                         href: 'https://earnfluencers.com/',
                     },
                 ],
             },
             {
-                class: 'col-span-3',
+				textColor: 'text-white',
+                class: 'col-span-3 bg-gradient-to-r from-[#302b63] to-[#24243e]',
+				imageClass: 'absolute w-[600px] right-16 bottom-0 !rounded-t-xl overflow-hidden',
                 title: 'Thingtrax',
                 slug: 'thingtrax',
                 image: pI_tt_image,
@@ -142,7 +152,7 @@ const services = [
                 ],
                 links: [
                     {
-                        name: 'Demo',
+                        name: 'Visit the website',
                         href: 'https://thingtrax.com/',
                     },
                 ],
@@ -154,55 +164,57 @@ const services = [
 function Card({ item }) {
     return (
         <div
-            className={`bg-primary-black text-white w-full h-[350px] border border-primary-black hover:border-gray-600 rounded-none flex justify-start flex-col items-start p-10 gap-5 ${item.class}`}>
-            {item.logo && (
-                <img src={item.logo} alt={`Project Logo: ${item.title}`} />
-            )}
-            <h1 className="text-white font-light font-poppins antialiased text-xl">
-                {item.title}
-            </h1>
-            <p className="text-white font-light font-poppins antialiased text-base">
-                {item.description}
-            </p>
+            className={`bg-primary-black overflow-hidden relative text-white w-full h-[350px] border border-primary-black hover:border-gray-600 rounded-none flex justify-start flex-row items-start p-10 gap-5 ${item.class} ${item.textColor}`}>
+            <div className="flex flex-col gap-4 w-[400px]">
+                <h1 className={`text-white font-bold font-poppins antialiased text-2xl ${item.textColor}`}>
+                    {item.title}
+                </h1>
+				<div
+                    className={`text-sm flex flex-row text-gray-700 text-left w-full font-poppins antialiased gap-3`}>
+                    {item.tech &&
+                        item.tech.map(tech => (
+                            <div className={`bg-black/30 text-white px-4 py-2`}>{tech.name}</div>
+                        ))}
+                </div>
+                <p className={`text-white font-light font-poppins antialiased text-lg ${item.textColor}`}>
+                    {item.description}
+                </p>
 
-            <div
-                className={`text-sm flex flex-row text-gray-700 text-left w-full font-poppins antialiased gap-3`}>
-                {item.tech &&
-                    item.tech.map(tech => (
-                        <div className={` `}>{tech.title}</div>
-                    ))}
+                <div className="flex flex-row gap-4">
+                    {item.links &&
+                        item.links.map(link => (
+                            <a
+                                href={link.href}
+                                target="_blank"
+                                className={`underline text-white underline-offset-[5px] flex flex-row gap-2 items-center ${item.textColor}`}
+                                rel="noreferrer">
+                                {link.name}{' '}
+                                <svg
+                                    width="16"
+                                    height="16"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M4.25 12H19"
+                                        stroke="currentColor"
+                                        stroke-width="2"
+                                        strokeLinecap="square"
+                                        strokeLinejoin="round"></path>
+                                    <path
+                                        d="M15 7.25L19.75 12L15 16.75"
+                                        stroke="currentColor"
+                                        stroke-width="2"
+                                        strokeLinecap="square"
+                                        strokeLinejoin="round"></path>
+                                </svg>
+                            </a>
+                        ))}
+                </div>
             </div>
-            <div className="flex flex-row gap-4">
-                {item.links &&
-                    item.links.map(link => (
-                        <a
-                            href={link.href}
-                            target="_blank"
-                            className="underline text-white underline-offset-[5px] flex flex-row gap-2 items-center"
-                            rel="noreferrer">
-                            {link.name}{' '}
-                            <svg
-                                width="16"
-                                height="16"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M4.25 12H19"
-                                    stroke="currentColor"
-                                    stroke-width="2"
-                                    strokeLinecap="square"
-                                    strokeLinejoin="round"></path>
-                                <path
-                                    d="M15 7.25L19.75 12L15 16.75"
-                                    stroke="currentColor"
-                                    stroke-width="2"
-                                    strokeLinecap="square"
-                                    strokeLinejoin="round"></path>
-                            </svg>
-                        </a>
-                    ))}
-            </div>
+			<div className={item.imageClass}>
+				<img src={item.image} className='w-full' />
+			</div>
         </div>
     )
 }
@@ -229,7 +241,7 @@ function ServicesPage() {
                 </Container>
             </div>
 
-            <Container className="h-full !justify-start !items-start mt-20 !flex-col gap-16">
+            <Container className="h-full !justify-start !items-start mt-10 !flex-col gap-4">
                 <h2 className="text-3xl text-white text-left">Our Projects</h2>
                 <div className="flex flex-col sm:grid sm:grid-cols-3 gap-8 justify-center items-center container cursor-pointer pb-10">
                     {service &&
