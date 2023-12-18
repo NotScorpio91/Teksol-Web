99ff8379191e41340d1113492d58af44fc19f2b9
[notscorpio91] <[notscorpio91@gmail.com]>
2023-12-18 00:28:03 -0800

    career page done

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { MdArrowForward } from 'react-icons/md'
 import NavBar from '../Components/NavBar'
 import MobileNavBar from '../Components/MobileNavBar'
 
@@ -7,7 +8,7 @@ import Footer from '../Components/Footer'
 function Career() {
     const Cards = [
         {
-            title: 'Senior Software Engineer - Developer Environments',
+            title: 'Senior Software Engineer  Developer Environments',
             text: 'San Francisco, CA or Remote (U.S.)',
         },
   
@@ -16,7 +17,7 @@ function Career() {
         <>
             <NavBar />
             <MobileNavBar />
-            <div className="flex justify-center items-center antialiased">
+            <div className="flex justify-center items-center antialiased px-4 sm:px-0">
                 <div className="flex flex-col  w-[1200px]">
                     <div>
                         <div className="flex flex-col justify-center items-center sm:pt-80 pt-28 sm:mx-0 mx-4 max-w-screen-xl">
@@ -36,23 +37,31 @@ function Career() {
                             <img src="" alt="" />
                         </div>
                     </div>
-                    <div className="pt-52">
+                    <div className="sm:pt-52 pt-20">
                         <div>
-                            <h1 className="text-[#FFF] font-poppins sm:text-4xl text-4xl leading-[80px] font-black cursor-text sm:pb-20 text-center antialiased">
+                            <h1 className="text-[#FFF] font-poppins sm:text-4xl text-2xl sm:leading-[80px] font-semibold cursor-text sm:pb-20 pb-12 text-center antialiased">
                                 We'd love to work with someone like you.
                             </h1>
                         </div>
                         <div className='flex flex-col sm:grid sm:grid-cols-3 gap-8 justify-center items-center container cursor-pointer sm:pb-10'>
                         {Cards.map(item => (
                           <div key={Cards} className='w-full'>
-                                <div className="bg-gradient-to-r from-primary-black to-black text-white w-full h-[350px] border border-primary-black hover:border-gray-600 rounded-none flex justify-start flex-col items-start p-10 gap-5 font-poppins ">
-                                    <h1 className="text-white font-light sm:font-semibold  antialiased text-xl sm:text-5xl">
+                                <div className="bg-gradient-to-r from-primary-black to-black text-white w-full sm:h-[350px] border border-primary-black hover:border-gray-600 rounded-none flex justify-center flex-col items-center py-10 sm:py-0  gap-5 font-poppins sm:px-7 px-3">
+                                  <div className="flex flex-col sm:justify-between justify-around">
+
+                                  <div className="">
+                                    <h1 className="text-white  font-semibold  antialiased text-3xl sm:text-5xl">
                                         {item.title}
                                     </h1>
-                                    
-                                    <p className="text-white font-light  antialiased text-base">
+                                  </div>
+                                    <div className="flex justify-start items-center pt-4 ">
+
+                                    <p className="text-white font-light  antialiased text-sm sm:text-base">
                                         {item.text}
                                     </p>
+                                        <MdArrowForward className='sm:ml-1' size={18} />
+                                    </div>
+                                </div>
                                 </div>
                             </div>
                         ))}
