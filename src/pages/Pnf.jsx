1f07717ab0bfb3f9bb70eef69011521c666f8451
notscorpio91 <notscorpio91.com>
2024-01-24 13:58:43 +0500

    pnf 404 page fixed

--- src/pages/Pnf.jsx
+++ src/pages/Pnf.jsx
@@ -25,13 +25,13 @@ export default function Pnf() {
                 <div className="blur-[106px] h-36 bg-gradient-to-t from-gray-500 to-gray-50 "></div>
             </div>
         <div className="text-center relative bottom-52 sm:bottom-60">
-          <p className="relative top-9 text-transparent bg-clip-text antialiased font-poppins text-5xl bg-gradient-to-r dark:from-[#00c6ff] dark:to-[#92EFFD]">404</p>
+          <p className="relative top-9 text-transparent bg-clip-text antialiased font-poppins text-7xl bg-gradient-to-r from-[#00c6ff] to-[#92EFFD]">404</p>
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-xl relative top-3">Page not found</h1>
           <p className="mt-6 text-base leading-7 text-white">Sorry, we couldn’t find the page you’re looking for.</p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
               href="/"
-              className="dark:text-black text-white dark:bg-gradient-to-r   font-medium rounded-md text-sm px-[30px] py-3 text-center  font-poppins antialiased transition-all active:scale-95 bg-gradient-to-r dark:from-[#00c6ff] dark:to-[#92EFFD]"
+              className="text-black    font-medium rounded-md text-sm px-[30px] py-3 text-center  font-poppins antialiased transition-all active:scale-95 bg-gradient-to-r from-[#00c6ff] to-[#92EFFD]"
             >
               Go back home
             </a>
