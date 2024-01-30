472443dbd5cdb0450307c918e6ce9eb28b7a72f3
notscorpio91 <notscorpio91.com>
2024-01-30 10:16:31 +0500

    Meeting card updated

--- src/Components/Bigcard.jsx
+++ src/Components/Bigcard.jsx
@@ -7,9 +7,9 @@ function Bigcard() {
     <div className='flex justify-center select-none font-poppins  px-4  '>
       <div  className=' dark:bg-gradient-to-r dark:from-blue-light dark:to-blue-dark  bg-white  md:w-[1080px] md:h-[425px] rounded-[20px] grid sm:grid-cols-2 justify-between   font-poppins text-gray-500 border border-gray-300   shadow-xl dark:shadow-none dark:border-none  '>
 
-        <div className="lhs flex flex-col  items-center text-center sm:items-start sm:text-start justify-center items p-8 w-full ">
+        <div className="lhs flex flex-col  items-center text-center sm:items-start sm:text-start justify-center items p-8 w-full  ">
         <h1 className=' w-[182px] h-[32px] dark:text-white text-gray-500  font-normal  antianalized  '>Love our Our Tool?</h1>
-        <div className='flex flex-col w-full sm:w-[499px] sm:h-[130px] pb-5'>
+        <div className='flex flex-col w-full sm:w-[499px] sm:h-[130px] pb-5 sm:space-y-5'>
         <h1 className=' dark:text-white text-black  font-semibold text-xl sm:text-5xl antianalized'>Fell Free to Join our</h1>
         <h1 className=' dark:text-white text-black  font-semibold text-xl sm:text-5xl'> 15 Days Free Trial</h1>
         </div>
--- src/pages/sections/Meeting.jsx
+++ src/pages/sections/Meeting.jsx
@@ -11,7 +11,7 @@ import Xiaomi from '../../assets/png/Xiaomi.png'
 
 function Meeting() {
   return (
-    <div className=' sm:my-32 my-10 sm:mb-[40pc] mb-64'>
+    <div className=' sm:my-32 my-10 sm:mb-72 mb-64'>
         <div className='sm:w-[1280px] sm:h-[625px] h-[400px]  flex flex-col justify-start items-center  mx-4 dark:bg-gradient-to-r dark:from-primary-black dark:to-black dark:text-white  border dark:border-primary-black dark:hover:border-gray-600 rounded-none  font-poppins text-gray-500 bg-gray-50 border-gray-300 hover:border-gray-400 shadow-xl dark:shadow-none'>
           <h1 className='dark:text-white text-black sm:leading-[64px] font-poppins antialiased font-semibold sm:text-[48px] px-8 sm:mt-20  sm:mb-10 mt-10 sm:my-0 text-base text-center'>TEKSOL Innovation Summit: Empowering Tomorrow's Tech Landscape</h1>
           <div className='grid grid-cols-3 sm:grid-cols-6 px-8  gap-10 my-8 sm:px-8  sm:my-0 items-center justify-center'>
